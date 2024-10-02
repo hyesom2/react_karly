56509f3e53d4979e6d6ac73bf29a24e7639dba3e
kimHyeonJu <kimhyesom1202@gmail.com>
2024-10-02 15:19:56 +0900

    Styles: Add fonts assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/* * @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -40,4 +40,6 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+  },
 }
