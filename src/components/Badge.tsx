13b4a0a2f6a38c3d3c2200cc7a482a06507b4a20
kimHyeonJu <kimhyesom1202@gmail.com>
2024-10-10 17:04:31 +0900

    Component: Add the "carn", "only", "limit" purpose properties of the Badge component

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 interface BadgeProps {
   purpose: string;
-  children: ReactNode;
+  children?: ReactNode;
 }
 
 function Badge({ children, purpose }: BadgeProps) {
@@ -21,13 +21,25 @@ function Badge({ children, purpose }: BadgeProps) {
       AddClassName += "text-primary bg-transparent border border-primary";
       break;
     }
+    case "earn": {
+      AddClassName += "text-white bg-accent";
+      break;
+    }
+    case "only": {
+      return (
+        <span className="inline-flex justify-center items-center label text-sm text-primary h-18 bg-gray-100 p-4 rounded">Karly Only</span>
+      )
+    }
+    case "limit": {
+      return (
+        <span className="inline-flex justify-center items-center label text-sm text-content h-18 bg-gray-100 p-4 rounded">한정수량</span>
+      )
+    }
   }
   return (
-    <>
-      <span className={ `inline-flex justify-center items-center text-sm label px-8 h-18 rounded-1 ${AddClassName}` }>
-        { children }
-      </span>
-    </>
+    <span className={ `inline-flex justify-center items-center text-sm label px-8 h-18 rounded-1 ${AddClassName}` }>
+      { children }
+    </span>
   )
 }
 
