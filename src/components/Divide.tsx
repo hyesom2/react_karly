3f9fbba6a7d858b20f8c7a9087e28d1483303a8a
kimHyeonJu <kimhyesom1202@gmail.com>
2024-10-15 17:54:21 +0900

    Refactor: Add Divide component for height 14 case & Remove divide.css file

--- src/components/Divide.tsx
+++ src/components/Divide.tsx
@@ -1,16 +1,21 @@
 interface DivideProps {
   height: number;
+  bgColor?: string;
 }
 
-function Divide({ height }: DivideProps) {
-  let divideClassName = "inline-block w-1 bg-content my-0";
+function Divide({ height, bgColor }: DivideProps) {
+  let divideClassName = "inline-block w-1 my-0";
   switch(height) {
     case 12: {
-      divideClassName += " h-12 mx-8";
+      divideClassName += ` h-12 mx-8 ${bgColor}`;
+      break;
+    }
+    case 14: {
+      divideClassName += ` h-14 mx-12 ${bgColor}`;
       break;
     }
     case 16: {
-      divideClassName += " h-16 mx-4";
+      divideClassName += ` h-16 mx-4 ${bgColor}`;
       break;
     }
   }
