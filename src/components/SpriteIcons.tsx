f187f0d06aef68ca201c2284f7d385ab5c3d2d52
kimHyeonJu <kimhyesom1202@gmail.com>
2024-10-15 14:43:32 +0900

    Component: Create Divide Component & Refactor SpriteIcons Component

--- src/components/SpriteIcons.tsx
+++ src/components/SpriteIcons.tsx
@@ -1,59 +1,63 @@
+import { useNavigate, Link } from 'react-router-dom'
+
 interface SpriteIconsProps {
   type: string,
 }
 
 function SpriteIcons({ type }: SpriteIconsProps) {
+  const navigate = useNavigate();
+
   switch (type) {
     case "logo": {
       return (
         <h1 className="icon-sprite logo" aria-label="마켓칼리 로고">
-          <a href="/"></a>
+          <Link to="/"></Link>
         </h1>
       )
     }
     case "blog": {
       return (
-        <a href="/" className="icon-sprite sns blog" aria-hidden="true"></a>
+        <Link to="/" className="icon-sprite sns blog" aria-hidden="true"></Link>
       )
     }
     case "facebook": {
       return (
-        <a href="https://www.facebook.com/Marketkurly/" target="_blank" className="icon-sprite sns facebook" aria-hidden="true"></a>
+        <Link to="https://www.facebook.com/Marketkurly/" target="_blank" className="icon-sprite sns facebook" aria-hidden="true"></Link>
       )
     }
     case "instagram": {
       return (
-        <a href="https://www.instagram.com/marketkurly" target="_blank" className="icon-sprite sns instagram" aria-hidden="true"></a>
+        <Link to="https://www.instagram.com/marketkurly" target="_blank" className="icon-sprite sns instagram" aria-hidden="true"></Link>
       )
     }
     case "naver-post": {
       return (
-        <a href="https://m.post.naver.com/marketkurly" target="_blank" className="icon-sprite sns naver-post" aria-hidden="true"></a>
+        <Link to="https://m.post.naver.com/marketkurly" target="_blank" className="icon-sprite sns naver-post" aria-hidden="true"></Link>
       )
     }
     case "youtube": {
       return (
-        <a href="https://www.youtube.com/channel/UCfpdjL5pl-1qKT7Xp4UQzQg" target="_blank" className="icon-sprite sns youtube" aria-hidden="true"></a>
+        <Link to="https://www.youtube.com/channel/UCfpdjL5pl-1qKT7Xp4UQzQg" target="_blank" className="icon-sprite sns youtube" aria-hidden="true"></Link>
       )
     }
     case "isms": {
       return (
-        <a href="/" className="icon-sprite footer isms" aria-hidden="true"></a>
+        <button type="button" className="icon-sprite footer isms" onClick={ () => navigate("/") }></button>
       )
     }
     case "privacy": {
       return (
-        <a href="/" className="icon-sprite footer privacy" aria-hidden="true"></a>
+        <button type="button" className="icon-sprite footer privacy" onClick={ () => navigate("/") }></button>
       )
     }
     case "toss-payments": {
       return (
-        <a href="/" className="icon-sprite footer toss-payments" aria-hidden="true"></a>
+        <button type="button" className="icon-sprite footer toss-payments" onClick={ () => navigate("/") }></button>
       )
     }
     case "woori-bank": {
       return (
-        <a href="/" className="icon-sprite footer woori-bank" aria-hidden="true"></a>
+        <button type="button" className="icon-sprite footer woori-bank" onClick={ () => navigate("/") }></button>
       )
     }
   }
